a521435a1f826d3291699ff19e59e41a980723a3
sheng <sheng@DESKTOP-EUPS8AJ.cdsafi.com>
2022-04-19 16:21:40 +0800

    123

---
+++ lib/src/main/java/jjj/SLF44.java
@@ -0,0 +1,20 @@
+package jjj;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+
+public class SLF44 {
+private static Logger LOGGER = LoggerFactory.getLogger(SLF44.class);
+
+
+
+public static void main(String[] args) {
+LOGGER.info("Hello World123");
+//System.out.println("Hello World");
+
+
+
+}
+}
